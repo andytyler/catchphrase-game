ccb91132bc90265e53f6651847847098531ec061
andytyler <atyler1@sheffield.ac.uk>
2019-08-28 21:13:03 +0100

    Added element getters

---
+++ public/signin.js
@@ -0,0 +1,10 @@
+const usernameElement = document.getElementById('username')
+const passwordElement = document.getElementById('password')
+const upUsernameElement = document.getElementById('upUsername')
+const upPasswordElement = document.getElementById('upPassword')
+
+const usernameText = usernameElement.innerText
+const passwordText = passwordElement.innerText
+const upUsernameText = upUsernameElement.innerText
+const upPasswordText = upPasswordElement.innerText
+
